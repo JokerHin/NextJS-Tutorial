ae2ae9c74015954f4d08ebd52237c175f7cec4d2
Cho Kar Hin <158465774+JokerHin@users.noreply.github.com>
2025-03-25 20:57:34 +0800

    Add files via upload

---
+++ Rendering/First render/About/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  console.log("This is a console log statement");
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
---
+++ Rendering/First render/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function DashboardPage() {
+  const [name, setname] = useState("");
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <input value={name} onChange={(e) => setname(e.target.value)} />
+      <p>Hello, {name} !</p>
+    </div>
+  );
+}
