f8eb65c5689fd0c95fb8cece17580518bb102eb3
Cho Kar Hin <158465774+JokerHin@users.noreply.github.com>
2025-03-26 22:02:56 +0800

    Add files via upload

---
+++ Data Fetching/normal data fetching/Fetching Data from Database/products-db/page.tsx
@@ -0,0 +1,33 @@
+import { getProducts } from "@/prisma-db";
+import { ProductDetail } from "./product-detail";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string | null;
+};
+
+export default async function ProductsPrismaDBPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const { query } = await searchParams;
+  const products: Product[] = await getProducts(query);
+
+  return (
+    <ul className="space-y-4 p-4">
+      {products.map((product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg front-medium">${product.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
